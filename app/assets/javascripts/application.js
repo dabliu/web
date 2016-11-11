a1ceb8b0dd3c52675c5ba6584e60e6c181813070
Nicolas Oga <2112.oga@gmail.com>
2016-11-10 23:04:05 -0300

    Fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
-//= require scriptGallery
 //= require jquery.min
 //= require bootstrap.min
 //= require jquery.easing.min
 //= require jquery.scrollTo
 //= require wow.min
 //= require custom
+//= require scriptGallery
